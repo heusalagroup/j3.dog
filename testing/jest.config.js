3f444c2e218309b87d1b0914b99b9552042778be
Jaakko Heusala <jhh@heusalagroup.fi>
2023-01-03 07:18:03 +0200

    Added initial Github CI to run unit tests

--- testing/jest.config.js
+++ testing/jest.config.js
@@ -1,10 +1,13 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   projects: [
-    "./src/fi/hg/core",
-    "./src/fi/hg/node",
-    "./src/fi/hg/backend",
-    "./src/fi/hg/frontend"
+    "./src/dog/j3/core",
+    "./src/dog/j3/backend",
+    "./src/dog/j3/frontend",
+    // "./src/fi/hg/core",
+    // "./src/fi/hg/node",
+    // "./src/fi/hg/backend",
+    // "./src/fi/hg/frontend"
   ],
   testTimeout: 300000
 };
